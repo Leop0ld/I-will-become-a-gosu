be328b665f206cef76bc6b01e4716943236833bc
Leop0ld <l3opold7@gmail.com>
2017-03-28 19:32:30 +0900

    Add description in 4, 5 files

--- go-tour-kr/go-tour-5.go
+++ go-tour-kr/go-tour-5.go
@@ -1,9 +1,16 @@
 package main
 
+/*
+이 코드에서는 여러개의 "package" 를 소괄호로 감싸서 import를 표현합니다. 아래와 같이 import 문장을 여러번 사용할 수 도 있습니다.
+ */
+
 import (
 	"fmt"
 	"math"
 )
+// import "fmt"
+// import "main"
+
 
 func main() {
 	fmt.Printf("Now you have %g problems.",
