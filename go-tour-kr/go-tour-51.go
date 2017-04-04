1ece2e1a159d96b86aaa5757ecc47eca0de62e38
Leop0ld <l3opold7@gmail.com>
2017-04-04 20:41:34 +0900

    Add 52, 53, 54 files and Modify 51 file

--- go-tour-kr/go-tour-51.go
+++ go-tour-kr/go-tour-51.go
@@ -5,6 +5,11 @@ import (
 	"math"
 )
 
+/*
+메소드는 아무 타입에나 붙일 수 있음
+다만 다른 패키지에 있는 타입이나 기본 타입들에 메소드를 붙이는 것은 불가능함
+*/
+
 type MyFloat float64
 
 func (f MyFloat) Abs() float64 {
---
+++ go-tour-kr/go-tour-52.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+/*
+메소드는 이름이 있는 타입 또는 이름이 있는 타입의 포인터와 연결 가능
+포인터 리시버를 사용하는 이유는 두 가지 입니다.
+첫째, 메소드가 호출될 때 마다 "값이 복사되는 것(큰 구조체 타입인 경우 값이 복사되는 것은 비효율적이므로)을 방지하기 위함" 입니다.
+다른 이유는 메소드에서 "리시버 포인터가 가르키는 값을 수정하기 위함" 입니다.
+*Vertex 타입의 리시버 대신 Vertex 를 사용하도록 메소드 Abs 와 Scale 의 선언부분을 바꿔 보세요.
+v 를 Vertex 타입으로 받으면 Scale 메소드가 더 이상 동작하지 않습니다. Scale 은 v 를 바꾸는데, v 가 (포인터가 아닌) 값 타입이기 때문에 Vertex 타입인 복사본에 작업을 하기 때문에 원래의 값은 바뀌지 않습니다.
+Abs 의 경우는 다릅니다. 여기서는 v 를 읽기만 하기 때문에, (포인터가 가르키는) 원래의 값이건 복사본이건 상관이 없게 됩니다.
+*/
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v *Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func (v *Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+func main() {
+	v := &Vertex{3, 4}
+	v.Scale(5)
+
+	fmt.Println(v, v.Abs())
+}
